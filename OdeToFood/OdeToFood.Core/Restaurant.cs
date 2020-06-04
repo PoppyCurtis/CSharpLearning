15d189f69ba99b582146d7b061858db5d9e408e6
PoppyCurtis <poppy.curtis@gmail.com>
2020-06-04 08:37:28 +0100

    reorg solution structure

---
+++ OdeToFood/OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
