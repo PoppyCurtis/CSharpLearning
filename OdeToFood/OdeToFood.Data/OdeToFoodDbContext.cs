1385dfb75ff8538df99b0d99a3d6fcf8e7ec2114
PoppyCurtis <poppy.curtis@gmail.com>
2020-06-11 08:39:56 +0100

    Add a connection string for SQL Server

--- OdeToFood/OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood/OdeToFood.Data/OdeToFoodDbContext.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
 
 namespace OdeToFood.Data
 {
-    class OdeToFoodDbContext
+    public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options )
+            : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,10 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
             services.AddRazorPages();
         }
