52ea8997bf709c4aff2efe78c11b6d632933f219
PoppyCurtis <poppy.curtis@gmail.com>
2020-06-14 18:49:34 +0100

    Create a scaffolded versoion 2 of the Restaurant page

---
+++ OdeToFood/OdeToFood/Pages/R2/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.R2
+{
+    public class CreateModel : PageModel
+    {
+        private readonly OdeToFood.Data.OdeToFoodDbContext _context;
+
+        public CreateModel(OdeToFood.Data.OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Restaurants.Add(Restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
