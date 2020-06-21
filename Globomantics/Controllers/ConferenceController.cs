8f440797fe5c3c8f909e6b0b3ec3b09a0948f484
PoppyCurtis <poppy.curtis@gmail.com>
2020-06-21 16:30:29 +0100

    Configure dependency injection with the configure method

---
+++ Globomantics/Controllers/ConferenceController.cs
@@ -0,0 +1,17 @@
+﻿using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomantics.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public ConferenceController(IConferenceService conferenceService)
+        {
+
+        }
+    }
+}
