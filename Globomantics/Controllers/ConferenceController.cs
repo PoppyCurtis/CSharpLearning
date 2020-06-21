3212f1f4a4bd0ef1a1b3e14e2d2cd212d9b43680
PoppyCurtis <poppy.curtis@gmail.com>
2020-06-21 18:09:44 +0100

    add css and update files using the Globomantics Task Runner

--- Globomantics/Controllers/ConferenceController.cs
+++ Globomantics/Controllers/ConferenceController.cs
@@ -1,10 +1,10 @@
-﻿using Globomantics.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Globomantics.Controllers
 {
--- Globomantics/Services/IConferenceService.cs
+++ Globomantics/Services/IConferenceService.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Globomantics.Services
 {
-    interface IConferenceService
+    public interface IConferenceService
     {
     }
 }
