6e636492083a5aebf20eb07427bc396d4da8e38c
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-05 14:54:14 +0100

    GET a single resource

--- RESTAPIProjct/Controllers/AuthorsController.cs
+++ RESTAPIProjct/Controllers/AuthorsController.cs
@@ -24,6 +24,13 @@ namespace RESTAPIProjct.Controllers
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
             return new JsonResult(authorsFromRepo);
         }
-
+        //parameter changes so is put between curly braces
+        //guid = route constraint to disambigouise between routes. Route will only match with input after authors can be matched to guid
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return new JsonResult(authorFromRepo);
+        }
     }
 }
