31d498634fca4676899bfb4e651f675cdff439b7
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-05 18:51:58 +0100

    Handle faults by sending a custom message when an HTTP 500 is returned

--- RESTAPIProjct/Startup.cs
+++ RESTAPIProjct/Startup.cs
@@ -46,7 +46,17 @@ namespace CourseLibrary.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler(appBuilder => 
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+            }
             app.UseRouting();
 
             app.UseAuthorization();
