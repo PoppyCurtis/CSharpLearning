aa9d5d795d33dc9c478e2ba80c0883d02f3b3720
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-05 18:41:38 +0100

    Return a single child resource

--- RESTAPIProjct/Controllers/CoursesController.cs
+++ RESTAPIProjct/Controllers/CoursesController.cs
@@ -34,5 +34,20 @@ namespace RESTAPIProjct.Controllers
             var coursesForAuthorFromRepo = _courseLibraryRepository.GetCourses(authorId);
             return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesForAuthorFromRepo));
         }
+        [HttpGet("{courseId}")]
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseForAuthorFromRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
+            if (courseForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<CourseDto>(courseForAuthorFromRepo));
+        }
     }
 }
