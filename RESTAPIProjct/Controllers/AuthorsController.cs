2a51263c67f1483681f1f9531f9663519863b93f
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-05 14:38:01 +0100

    Implement the outer facing contract of API

---
+++ RESTAPIProjct/Controllers/AuthorsController.cs
@@ -0,0 +1,29 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPIProjct.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+    }
+}
