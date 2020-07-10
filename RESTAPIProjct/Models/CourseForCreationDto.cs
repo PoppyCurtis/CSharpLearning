ebc81853073190069b6adb023acd263c3a08eb32
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-10 07:22:14 +0100

    Validate data input with data annotations. Add a required attribute and set maximum lengths for different fiels

--- RESTAPIProjct/Models/CourseForCreationDto.cs
+++ RESTAPIProjct/Models/CourseForCreationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace RESTAPIProjct.Models
 {
     public class CourseForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
