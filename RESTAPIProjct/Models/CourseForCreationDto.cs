ae82fae1462632b8e805329843ee8b793786359c
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-10 07:36:15 +0100

    Implement class-input validaition with IValidatableObject and set a custom rule that if the title is equal to a description then an error is thrown

--- RESTAPIProjct/Models/CourseForCreationDto.cs
+++ RESTAPIProjct/Models/CourseForCreationDto.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace RESTAPIProjct.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
         [Required]
         [MaxLength(100)]
         public string Title { get; set; }
         [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from the title.",
+                    new[] { "CourseForCreationDto" });
+            }
+        }
     }
 }
