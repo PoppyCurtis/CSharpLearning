84aa19f4664a24397bd0dff9a972c3c2e2510d07
PoppyCurtis <poppy.curtis@gmail.com>
2020-07-13 08:17:35 +0100

    Delete a resource with child resources

--- RESTAPIProjct/Controllers/AuthorsController.cs
+++ RESTAPIProjct/Controllers/AuthorsController.cs
@@ -67,5 +67,19 @@ namespace RESTAPIProjct.Controllers
             Response.Headers.Add("Allow", "GET,OPTIONS,POST");
             return Ok();
         }
+
+        [HttpDelete("{authorId}")]
+        public ActionResult DeleteAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+            _courseLibraryRepository.DeleteAuthor(authorFromRepo);
+            _courseLibraryRepository.Save();
+            return NoContent();
+        }
     }
 }
